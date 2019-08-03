da8a62e566e427290edd5773487a428f3f5ca8a9
cdrch <carterdavidhenning@yahoo.com>
2019-08-03 03:23:19 -0400

    Add basic working Tetris clone

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Block : MonoBehaviour
+{
+    public Vector3 rotationPoint;
+
+    private float previousTime;
+    public float fallTime = 0.8f;
+    public float forcedFallTime = 0.08f;
+
+    public static int height = 20;
+    public static int width = 10;
+    private static Transform[,] grid = new Transform[width, height];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var keyboard = Keyboard.current;
+        if (keyboard == null)
+        {
+            Debug.Log("null keyboard");
+            return;
+        }
+
+        if (keyboard.leftArrowKey.wasPressedThisFrame)
+        {
+            transform.position += new Vector3(-1, 0, 0);
+            if (!ValidMove())
+                transform.position -= new Vector3(-1, 0, 0);
+        }
+        else if (keyboard.rightArrowKey.wasPressedThisFrame)
+        {
+            transform.position += new Vector3(1, 0, 0);
+            if (!ValidMove())
+                transform.position -= new Vector3(1, 0, 0);
+        }
+        else if (keyboard.upArrowKey.wasPressedThisFrame)
+        {
+            transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
+            if (!ValidMove())
+                transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
+        }
+
+        if (Time.time - previousTime > (keyboard.downArrowKey.isPressed ? forcedFallTime : fallTime))
+        {
+            transform.position += new Vector3(0, -1, 0);
+            if (!ValidMove())
+            {
+                transform.position -= new Vector3(0, -1, 0);
+                AddToGrid();
+                CheckForLines();
+                this.enabled = false;
+                FindObjectOfType<SpawnTetromino>().SpawnNewTetromino();
+            }                
+            previousTime = Time.time;
+        }
+    }
+
+    private void CheckForLines()
+    {
+        for (int i = 0; i < height; i++)
+        {
+            if(HasLine(i))
+            {
+                DeleteLine(i);
+                RowDown(i);
+            }
+        }
+    }
+
+    private bool HasLine(int i)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (grid[j, i] == null)
+                return false;
+        }
+
+        return true;
+    }
+
+    private bool DeleteLine(int i)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            Destroy(grid[j, i].gameObject);
+            grid[j, i] = null;
+        }
+
+        return true;
+    }
+
+    private void RowDown(int i)
+    {
+        for (int y = i; y < height; y++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (grid[j, y] != null)
+                {
+                    grid[j, y - 1] = grid[j, y];
+                    grid[j, y] = null;
+                    grid[j, y - 1].transform.position -= new Vector3(0, 1, 0);
+                }                
+            }
+        }
+    }
+
+    private void AddToGrid()
+    {
+        foreach (Transform child in transform)
+        {
+            int roundedX = Mathf.RoundToInt(child.transform.position.x);
+            int roundedY = Mathf.RoundToInt(child.transform.position.y);
+
+            grid[roundedX, roundedY] = child;
+        }
+    }
+
+    public bool ValidMove()
+    {
+        foreach (Transform child in transform)
+        {
+            int roundedX = Mathf.RoundToInt(child.transform.position.x);
+            int roundedY = Mathf.RoundToInt(child.transform.position.y);
+
+            if (roundedX < 0 || roundedX >= width || roundedY < 0 || roundedY >= height)
+            {
+                return false;
+            }
+
+            if (grid[roundedX, roundedY] != null)
+                return false;
+        }
+
+        return true;
+    }
+}
---
+++ Assets/Scripts/SpawnTetromino.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTetromino : MonoBehaviour
+{
+    public GameObject[] tetrominoes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnNewTetromino();
+    }
+
+    public void SpawnNewTetromino()
+    {
+        GameObject tetromino = Instantiate(tetrominoes[Random.Range(0, tetrominoes.Length)], transform.position, Quaternion.identity);
+        if(!tetromino.GetComponent<Block>().ValidMove()) // if blocked on spawn
+        {
+            Destroy(tetromino);
+            Debug.Log("Game Over!");
+            this.enabled = false;
+        }
+    }
+}
